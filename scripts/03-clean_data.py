421eca568f6db8b51adfdc42de8e14136dce8c4b
JGilbert <j.gilbert@mail.utoronto.ca>
2024-10-22 19:40:53 -0400

    code and csv for swing state polls > 2.5 grade

--- scripts/03-clean_data.py
+++ scripts/03-clean_data.py
@@ -12,6 +12,23 @@ import pandas as pd
 
 raw_df = pd.read_csv("data/raw_data/president_polls.csv")
 
-print(raw_df.dtypes)
+
 # restrict to wuality polls only
-df = raw_df.loc(raw_df['numeric_grade'] > 2.7)
+df = raw_df.loc[raw_df['numeric_grade'] >= 2.5]
+df['exam_date'] = pd.to_datetime(df['end_date'], format='%m/%d/%y')
+
+df_swing = df.loc[df["state"].isin(["Arizona","Pennsylvania", "North Carolina", "Georgia", "Nevada", "Michigan", "Wisconsin"])]
+print(df.count())
+unique_values = df_swing['state'].unique()
+
+# Print unique values and their counts
+print("Unique values:")
+for value in unique_values:
+    count = (df_swing['state'] == value).sum()
+    print(f"{value}: {count}")
+
+print(df_swing["numeric_grade"].min())
+
+df_swing.to_csv("data/raw_data/swing_state_polls.csv")
+
+
