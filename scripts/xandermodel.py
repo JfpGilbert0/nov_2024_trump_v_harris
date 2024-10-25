115e354d1d2a14eeedc29216741e9b27e7b3d1b4
JGilbert <j.gilbert@mail.utoronto.ca>
2024-10-24 22:19:14 -0400

    Vote pct mean table

--- scripts/xandermodel.py
+++ scripts/xandermodel.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
 # Load the cleaned polling data
-clean_poll_data = pd.read_csv("data/02-analysis_data/merged_swing_state_data.parquet")
+clean_poll_data = pd.read_csv("data/02-analysis_data/merged_swing_state_data.csv")
 
 # Create a weight for each poll based on recency and sample size
 # For simplicity, let's assume that the weight is based on the sample size directly
